3296a6e5d725c80361cc7f5de3be30d158b2568c
sandra <sps166@educa.madrid.org>
2026-05-26 10:03:03 +0200

    implementar JavaFXNavigationService

--- src/main/java/catadopt/service/navigation/JavaFXNavigationService.java
+++ src/main/java/catadopt/service/navigation/JavaFXNavigationService.java
@@ -1,8 +1,18 @@
 package catadopt.service.navigation;
 
+import java.io.IOException;
+
+import catadopt.controller.AdoptedCatsController;
+import catadopt.controller.BrowserController;
+import catadopt.controller.NamePromptController;
+import catadopt.controller.WelcomeController;
 import catadopt.service.CatApiService;
 import catadopt.service.DatabaseService;
 import catadopt.service.NavigationService;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 // Sandra
@@ -18,24 +28,85 @@ public class JavaFXNavigationService implements NavigationService {
 		this.apiService = apiService;
 		this.databaseService = databaseService;
 	}
+	
+	@Override
+	public void loadScene(String fxmlFile) {
+		try {
+			// Asumiendo los FXML  están en la carpeta de recursos
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+			Parent root = loader.load();
+
+			// Obtener el controlador asignado en el FXML
+			Object controller = loader.getController();
+
+			// Inyección dinámica de dependencias según el controlador cargado
+			if (controller instanceof WelcomeController) {
+				WelcomeController welcomeCtrl = (WelcomeController) controller;
+				welcomeCtrl.setNavigationService(this);
+			} else if (controller instanceof BrowserController) {
+				BrowserController browserCtrl = (BrowserController) controller;
+				browserCtrl.setNavigationService(this);
+				browserCtrl.setApiService(this.apiService);
+			} else if (controller instanceof AdoptedCatsController) {
+				AdoptedCatsController adoptedCtrl = (AdoptedCatsController) controller;
+				adoptedCtrl.setNavigationService(this);
+				adoptedCtrl.setDatabaseService(this.databaseService);
+			}
+
+			// Cambiar la escena del Stage principal
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+		} catch (IOException e) {
+			System.err.println("Error al cargar la vista: " + fxmlFile);
+			e.printStackTrace();
+		}
+	}
+		
     @Override
     public void goToWelcome() {
-    	// TODO implementar
+    	loadScene("welcome_screen.fxml");
     }
 
     @Override
     public void goToBrowser() {
-    	// TODO implementar
+    	loadScene("browser_view.fxml");
     }
 
     @Override
     public void goToAdoptedCats() {
-    	// TODO implementar
+    	loadScene("adopted_cats_view.fxml");
     }
 
     @Override
     public String promptForName() {
-    	// TODO implementar
-        return null;
+    	 try {
+         	// Carga del diálogo 
+             FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/name_prompt_dialog.fxml"));
+             Parent root = loader.load();
+
+          // Crear una nueva ventana (Stage) secundaria para el diálogo
+             Stage dialogStage = new Stage();
+             dialogStage.setTitle("Nombre del Gatito");
+             dialogStage.initModality(Modality.WINDOW_MODAL);
+             dialogStage.initOwner(stage);
+             
+             Scene scene = new Scene(root);
+             dialogStage.setScene(scene);
+
+             // Obtener el controlador del diálogo para extraer el nombre ingresado
+             NamePromptController controller = loader.getController();
+             controller.setDialogStage(dialogStage);
+
+             dialogStage.showAndWait(); // Pausa la ejecución hasta que se cierre la ventana
+
+          
+             return controller.getEnteredName();// Devuelve el texto definitivo 
+         } catch (IOException e) {
+         	System.err.println("Error al abrir el diálogo name_prompt_dialog.fxml");
+             e.printStackTrace();
+             return null;
+         }
+     
     }
+
 }
