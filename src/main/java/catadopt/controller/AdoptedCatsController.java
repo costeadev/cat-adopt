5aea805fd09fec3ad11a20f1df16a87eda014f57
sandra <sps166@educa.madrid.org>
2026-05-26 11:56:37 +0200

    Implementar logica de controladores

--- src/main/java/catadopt/controller/AdoptedCatsController.java
+++ src/main/java/catadopt/controller/AdoptedCatsController.java
@@ -1,6 +1,62 @@
 package catadopt.controller;
 
+import java.util.List;
+
+import catadopt.model.Cat;
+import catadopt.service.DatabaseService;
+import catadopt.service.NavigationService;
+import javafx.fxml.FXML;
+import javafx.scene.layout.FlowPane;
+
 // Alberto
 public class AdoptedCatsController {
+	
+	private NavigationService navigationService;
+
+    private DatabaseService databaseService;
+
+    @FXML
+    private FlowPane adoptedCatsContainer;
+
+    /**
+     * Inyección navegación.
+     */
+    public void setNavigationService(
+            NavigationService navigationService
+    ) {
+        this.navigationService = navigationService;
+    }
+
+    /**
+     * Inyección base de datos.
+     */
+    public void setDatabaseService(
+            DatabaseService databaseService
+    ) {
+        this.databaseService = databaseService;
+    }
+
+    /**
+     * Carga gatos adoptados.
+     */
+    public void loadAdoptedCats() {
+
+        List<Cat> adoptedCats =
+                databaseService.getAdoptedCats();
+
+        for (Cat cat : adoptedCats) {
+
+            System.out.println(cat.getName());
+        }
+    }
+
+    /**
+     * Botón volver.
+     */
+    @FXML
+    private void onBackClick() {
+
+        navigationService.goToBrowser();
+    } 
 
 }
--- src/main/java/catadopt/controller/NamePromptController.java
+++ src/main/java/catadopt/controller/NamePromptController.java
@@ -1,6 +1,47 @@
 package catadopt.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 // Alberto
 public class NamePromptController {
 
+	@FXML
+	private TextField nameField;
+	private Stage dialogStage;
+	private String catName;
+
+	/**
+     * Acción del botón confirmar.
+     */
+	@FXML
+	private void onConfirm() {
+		catName = nameField.getText();
+		dialogStage.close();
+	}
+
+	 /**
+     * Acción del botón cancelar.
+     */
+    @FXML
+    private void onCancel() {
+        catName = null;
+        dialogStage.close();
+    }
+
+    /**
+     * Recibe referencia del diálogo.
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    /**
+     * Devuelve nombre introducido.
+     */
+    public String getCatName() {
+        return catName;
+    }
+	
 }
